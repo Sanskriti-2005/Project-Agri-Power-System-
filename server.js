1acbf9ba7e6b442dcf44a7573556b29673414496
Sanskriti <sanskriti121105@gmail.com>
2026-04-24 10:49:00 +0530

    Test commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ let userSchema = mongoose.Schema({
     role: String,
     password: String   
 })
-
+fgh
 let User = mongoose.model("users", userSchema)
 
 mongoose.connect("mongodb://127.0.0.1:27017/agricultureDB")
